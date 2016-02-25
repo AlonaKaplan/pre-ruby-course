f586eb236e549bdd55b10cdf7455a1cf7f6590b6
Alona Kaplan <alkaplan@redhat.com>
2016-02-25 19:32:21 +0200

    ruby course: First assignment

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,18 @@
 class StringExt
+
+  def initialize (str1, str2)
+    @str1 = str1
+    @str2 = str2
+  end
+
+  def concat
+   return @str1 + @str2
+  end
+
+  def substract
+   tmp = @str1
+   tmp.slice! @str2
+   return tmp 
+  end
+
 end
